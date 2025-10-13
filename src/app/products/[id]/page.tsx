8ed844c8095a6063fb9cde05b9cc2c0ac5cd5f53
keshav <keshav.kattel@waterflow.technology>
2025-10-13 10:33:22 +0545

    fix single product page issue

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,9 +1,8 @@
 import ProductInteraction from "@/components/ProductInteraction";
-import type { ProductType } from "@/types";
+import { ProductType } from "@/types";
 import Image from "next/image";
-import React from "react";
 
-// TEMPORARY PRODUCT DATA
+// TEMPORARY
 const product: ProductType = {
   id: 1,
   name: "Adidas CoreFit T-Shirt",
@@ -11,8 +10,8 @@ const product: ProductType = {
     "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
   description:
     "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
-  price: 39.9,
-  sizes: ["s", "m", "l", "xl", "xxl"],
+  price: 59.9,
+  sizes: ["xs", "s", "m", "l", "xl"],
   colors: ["gray", "purple", "green"],
   images: {
     gray: "/products/1g.png",
@@ -41,12 +40,13 @@ const ProductPage = async ({
   params: Promise<{ id: string }>;
   searchParams: Promise<{ color: string; size: string }>;
 }) => {
-  const { color, size } = await searchParams;
-  const selectedColor = color || product.colors[0];
-  const selectedSize = size || product.sizes[0];
+  const { size, color } = await searchParams;
+
+  const selectedSize = size || (product.sizes[0] as string);
+  const selectedColor = color || (product.colors[0] as string);
   return (
-    <div className="flex flex-col lg:flex-row gap-8 md:12 mt-12">
-      {/* IMAGE  */}
+    <div className="flex flex-col gap-4 lg:flex-row md:gap-12 mt-12">
+      {/* IMAGE */}
       <div className="w-full lg:w-5/12 relative aspect-[2/3]">
         <Image
           src={product.images[selectedColor]}
@@ -55,19 +55,16 @@ const ProductPage = async ({
           className="object-contain rounded-md"
         />
       </div>
-      {/* DETAILS  */}
+      {/* DETAILS */}
       <div className="w-full lg:w-7/12 flex flex-col gap-4">
-        <h1 className="font-medium text-2xl">{product.name}</h1>
+        <h1 className="text-2xl font-medium">{product.name}</h1>
         <p className="text-gray-500">{product.description}</p>
-        <p className="text-2xl font-semibold">${product.price?.toFixed(2)}</p>
-
-        {/* interaction  */}
+        <h2 className="text-2xl font-semibold">${product.price.toFixed(2)}</h2>
         <ProductInteraction
           product={product}
-          selectedColor={selectedColor}
           selectedSize={selectedSize}
+          selectedColor={selectedColor}
         />
-
         {/* CARD INFO */}
         <div className="flex items-center gap-2 mt-4">
           <Image
@@ -94,19 +91,11 @@ const ProductPage = async ({
         </div>
         <p className="text-gray-500 text-xs">
           By clicking Pay Now, you agree to our{" "}
-          <span className="underline hover:text-black cursor-pointer">
-            Terms & Conditions
-          </span>{" "}
-          and{" "}
-          <span className="underline hover:text-black cursor-pointer">
-            Privacy Policy
-          </span>
+          <span className="underline hover:text-black">Terms & Conditions</span>{" "}
+          and <span className="underline hover:text-black">Privacy Policy</span>
           . You authorize us to charge your selected payment method for the
           total amount shown. All sales are subject to our return and{" "}
-          <span className="underline hover:text-black cursor-pointer">
-            Refund Policies
-          </span>
-          .
+          <span className="underline hover:text-black">Refund Policies</span>.
         </p>
       </div>
     </div>
